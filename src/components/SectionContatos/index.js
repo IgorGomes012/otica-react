0e0be4a3da9044840ea89e2bacc95768286a9737
igorGomes <gomesoliveirasl02@hotmail.com>
2022-06-22 12:22:42 -0300

    projeto estilizado ainda sem reponsividade

--- src/components/SectionContatos/index.js
+++ src/components/SectionContatos/index.js
@@ -1,32 +1,33 @@
 import React from 'react';
+import "./style.css";
 
-export default function SectionContatos(){
-  return(
+export default function SectionContatos() {
+  return (
     <section id='section-contatos'>
-      <div>
-        <h2>FALE CONOSCO</h2>
-        <p>não perca tempo, venha conhecer uma das nossas lojas ou entre em contato através de nossas redes sociais ou da central de atendimento</p>
-        <div>
-             <div>
-                <h2>Contato</h2>
-                <div>
-                <p> <img src='../assets/local.png' alt=''/> São Lourenço, MG</p>
-                <p> <img src='../assets/telefone.png' alt=''/> (35)991725264</p>
-                <p> <img src='../assets/email.png' alt=''/> igoroliveirasl0210@gmail.com</p>
-                </div>
-             </div>
+      <div className="limitar-section-contatos">
+        <h2 className='title-contatos'>FALE CONOSCO</h2>
+        <p className='title-contatos'>Não perca tempo, venha conhecer uma das nossas lojas ou entre em contato através de nossas redes sociais ou da central de atendimento</p>
+        <div className='element-container'>
+          <div className='element-contatos'>
+            <h2>Contato</h2>
+            <div>
+              <div className='img-text-contatos'> <p> <img src='../assets/local.png' alt='' /> São Lourenço, MG</p></div>
+              <div className='img-text-contatos'>  <p> <img src='../assets/telefone.png' alt='' /> (35)991725264</p></div>
+              <div className='img-text-contatos'> <p> <img src='../assets/email.png' alt='' /> igoroliveirasl0210@gmail.com</p></div>
+            </div>
+          </div>
 
-             <div>
-             <h2>Nossas Redes Sociais</h2>
-                <div>
-                <p><img src='../assets/fb.png' alt=''/> IgorGomes</p>
-                <p> <img src='../assets/ig.png' alt=''/> IgorGomes02</p>
-                <p>  <img src='../assets/tt.png' alt=''/> IgorGomes02</p>
-                </div>
-             </div>
-
+          <div className='element-contatos'>
+            <h2>Nossas Redes Sociais</h2>
+            <div>
+              <div className='img-text-contatos'> <p><img src='../assets/fb.png' alt='' /> IgorGomes</p></div>
+              <div className='img-text-contatos'> <p> <img src='../assets/ig.png' alt='' /> IgorGomes02</p></div>
+            <div className='img-text-contatos'> <p>  <img src='../assets/tt.png' alt='' /> IgorGomes02</p></div>
+          </div>
         </div>
+
       </div>
-    </section>
+    </div>
+    </section >
   )
 }
--- src/components/SectionSobre/index.js
+++ src/components/SectionSobre/index.js
@@ -4,21 +4,27 @@ import "./style.css"
 export default function SectionSobre() {
   return (
     <section id="section-sobre">
-      <div >
+      <div className='limitar-secao-sobre'>
         <h1>QUEM SOMOS NÓS?</h1>
         <p>Fundada em 2001 em São lourenço - Minas Gerais, a Ótica Gomes iniciou suas atividades focada no atendimento ao publico de renda mais baixa, sempre com o objetivo de proporcionar ao cliente bom atendimento, qualidade e preço baixo.</p>
         <div id='secao-sobre'>
-          <img src='../assets/loja.png' alt='' />
-          <div>
+
+          <div className="cards-sobre" id='img1-sobre-div'>
+             <img src='../assets/atendimento.png' id='img1-sobre'></img>
+          </div>
+
+          <div className="cards-sobre" id='card-texto-sobre1'>
             <h1>NOSSA FILIAIS</h1>
             <p>Hoje temos mais de 20 filiais pelo Brasil e na América</p>
           </div>
 
-          <img src='../assets/atendimento.png' alt='' />
-
-          <div>
+          <div className="cards-sobre" id='card-texto-sobre2'>
             <h1>ATENDIMENTO FLEXIVEL</h1>
             <p>Nossa equipe é treinada para te atender</p>
+          </div>
+
+          <div className="cards-sobre" id='img2-sobre-div'>
+            <img src='../assets/loja.png' id='img2-sobre'></img>
           </div>
 
         </div>
